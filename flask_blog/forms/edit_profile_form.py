5be59f66b74ada39d16bbcab4478ea96421ffc84
Yugbhuva <ybhuva817@gmail.com>
2025-07-22 17:43:07 +0530

    Huge Update

--- flask_blog/forms/edit_profile_form.py
+++ flask_blog/forms/edit_profile_form.py
@@ -1,9 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, ValidationError
+from flask_login import current_user
+from flask_blog.app import mongo
 
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     bio = TextAreaField('Bio', validators=[Length(max=500)])
-    submit = SubmitField('Update Profile') 
+    submit = SubmitField('Update Profile')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = mongo.db.users.find_one({'username': username.data})
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = mongo.db.users.find_one({'email': email.data})
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.') 
--- flask_blog/routes/auth.py
+++ flask_blog/routes/auth.py
@@ -2,12 +2,14 @@ from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required, current_user
 from urllib.parse import urlparse
 from datetime import datetime
+from bson.objectid import ObjectId
 
-from flask_blog.models.user import User
+from flask_blog.models.user import MongoUser, create_user
 from flask_blog.forms.login_form import LoginForm
 from flask_blog.forms.register_form import RegisterForm
 from flask_blog.forms.delete_account_form import DeleteAccountForm
-from flask_blog.app import db
+from flask_blog.forms.edit_profile_form import EditProfileForm
+from flask_blog.app import mongo
 
 auth = Blueprint('auth', __name__)
 
@@ -18,53 +20,41 @@ def login():
     
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user is None or not user.check_password(form.password.data):
+        user_doc = mongo.db.users.find_one({'email': form.email.data})
+        if not user_doc or not MongoUser(user_doc).check_password(form.password.data):
             flash('Invalid email or password', 'danger')
             return redirect(url_for('auth.login'))
-        
+        user = MongoUser(user_doc)
         login_user(user, remember=form.remember_me.data)
-        user.last_login = datetime.utcnow()
-        db.session.commit()
-        
+        mongo.db.users.update_one({'_id': user_doc['_id']}, {'$set': {'last_login': datetime.utcnow()}})
         next_page = request.args.get('next')
         if not next_page or urlparse(next_page).netloc != '':
             next_page = url_for('blog.index')
-        
         flash('You have been logged in successfully!', 'success')
         return redirect(next_page)
-    
     return render_template('login.html', title='Sign In', form=form)
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('blog.index'))
-    
     form = RegisterForm()
     if form.validate_on_submit():
         # Check if username or email already exists
-        if User.query.filter_by(username=form.username.data).first():
+        if mongo.db.users.find_one({'username': form.username.data}):
             flash('Username already exists. Please choose a different one.', 'danger')
             return redirect(url_for('auth.register'))
-        
-        if User.query.filter_by(email=form.email.data).first():
+        if mongo.db.users.find_one({'email': form.email.data}):
             flash('Email already registered. Please use a different one.', 'danger')
             return redirect(url_for('auth.register'))
-        
-        user = User(
+        user = create_user(
             username=form.username.data,
             email=form.email.data,
             password=form.password.data,
             bio=form.bio.data
         )
-        
-        db.session.add(user)
-        db.session.commit()
-        
         flash('Your account has been created! You can now login.', 'success')
         return redirect(url_for('auth.login'))
-    
     return render_template('register.html', title='Register', form=form)
 
 @auth.route('/logout')
@@ -74,33 +64,53 @@ def logout():
     flash('You have been logged out.', 'info')
     return redirect(url_for('blog.index'))
 
-@auth.route('/profile', methods=['GET', 'POST'])
+@auth.route('/profile')
 @login_required
 def profile():
     form = DeleteAccountForm()
     return render_template('profile.html', title='My Profile', form=form)
 
+@auth.route('/profile/edit', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user_doc = mongo.db.users.find_one({'_id': ObjectId(current_user.id)})
+        user_model = MongoUser(current_user_doc)
+        
+        update_data = {
+            'username': form.username.data,
+            'email': form.email.data,
+            'bio': form.bio.data
+        }
+        
+        mongo.db.users.update_one({'_id': ObjectId(current_user.id)}, {'$set': update_data})
+        
+        flash('Your profile has been updated!', 'success')
+        return redirect(url_for('auth.profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+        form.bio.data = current_user.bio
+        
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
+
 @auth.route('/delete-account', methods=['POST'])
 @login_required
 def delete_account():
     form = DeleteAccountForm()
+    if not current_user.is_authenticated:
+        flash('You must be logged in to delete your account.', 'danger')
+        return redirect(url_for('auth.login'))
     if form.validate_on_submit():
-        # Verify password before allowing account deletion
-        if not current_user.check_password(form.password.data):
+        user_doc = mongo.db.users.find_one({'_id': ObjectId(current_user.id)})
+        if not user_doc or not MongoUser(user_doc).check_password(form.password.data):
             flash('Incorrect password. Account deletion cancelled.', 'danger')
             return redirect(url_for('auth.profile'))
-        
-        user = User.query.get(current_user.id)
-        # Log the user out first
+        user_id = current_user.id
         logout_user()
-        
-        # Delete the user (cascading will handle related posts and comments)
-        db.session.delete(user)
-        db.session.commit()
-        
+        mongo.db.users.delete_one({'_id': ObjectId(user_id)})
         flash('Your account has been permanently deleted.', 'success')
         return redirect(url_for('blog.index'))
-    
-    # If form validation fails
     flash('Account deletion failed. Please check your password.', 'danger')
     return redirect(url_for('auth.profile'))
